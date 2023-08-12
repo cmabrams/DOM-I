dfeb1dc3b8142e0a9f29dc5b0f1eacd3662d604d
Cristina Abrams <cristina.abrams3@gmail.com>
2023-08-12 12:47:42 -0400

    answered all questions in js file.

--- src/index.js
+++ src/index.js
@@ -39,4 +39,79 @@ const siteContent = { // DO NOT CHANGE THIS OBJECT
   },
 };
 
-console.log('project wired!')
+console.log('Project is Wired');
+
+const anchorTags = document.getElementsByTagName('a');
+const items = ['Services', 'Product', 'Vision', 'Features', 'About', 'Contact'];
+
+for (let i = 0; i < items.length; i++) {
+  const tag = anchorTags[i];
+  const item = items[i];
+  tag.classList.add('italic');
+  tag.textContent = item;
+}
+
+const logoImg = document.querySelector('#logo-img');
+logoImg.src = 'http://localhost:9000/img/logo.png';
+
+const mainCtaHeading = document.querySelector('h1');
+mainCtaHeading.textContent = 'DOM Is Awesome';
+
+const mainCtaButton = document.querySelector('button');
+mainCtaButton.textContent = 'Get Started';
+
+const ctaImg = document.querySelector('#cta-img');
+ctaImg.src = 'http://localhost:9000/img/cta.png';
+
+const txtContentInsideTopContent = document.querySelectorAll('.top-content .text-content');
+const firstDiv = txtContentInsideTopContent[0];
+const headingFirst = firstDiv.querySelector('h4');
+console.log(headingFirst);
+headingFirst.textContent = 'Features';
+const pFirst = firstDiv.querySelector('p');
+pFirst.textContent = 'Features content elementum magna eros, ac posuere elvit tempus et. Suspendisse vel tempus odio, in interdutm nisi. Suspendisse eu ornare nisl. Nullam convallis augue justo, at imperdiet metus scelerisque quis.'
+const secondDiv = txtContentInsideTopContent[1];
+const secondHeading = secondDiv.querySelector('h4');
+secondHeading.textContent = 'About';
+const pSecond = secondDiv.querySelector('p');
+pSecond.textContent = 'About content elementum magna eros, ac posuere elvit tempus et. Suspendisse vel tempus odio, ininterdutm nisi. Suspendisse eu ornare nisl. Nullam convallis augue justo, at imperdiet metus scelerisque quis.';
+
+const midImg = document.querySelector('#middle-img');
+midImg.src = 'http://localhost:9000/img/accent.png';
+
+const textContentInsideBottomContent = document.querySelectorAll('.bottom-content .text-content');
+
+const firstBottomDiv = textContentInsideBottomContent[0];
+const firstBottomHeading = firstBottomDiv.querySelector('h4');
+firstBottomHeading.textContent = 'Services';
+const firstBottomP = firstBottomDiv.querySelector('p');
+firstBottomP.textContent = 'Services content elementum magna eros, ac posuere elvit tempus et. Suspendisse vel tempus odio, in interdutm nisi. Suspendisse eu ornare nisl. Nullam convallis augue justo, at imperdiet metus scelerisque quis.';
+
+const secondBottomDiv = textContentInsideBottomContent[1];
+const secondBottomHeading = secondBottomDiv.querySelector('h4');
+secondBottomHeading.textContent = 'Product';
+const secondBottomP = secondBottomDiv.querySelector('p');
+secondBottomP.textContent = 'Product content elementum magna eros, ac posuere elvit tempus et. Suspendisse vel tempus odio, in interdutm nisi. Suspendisse eu ornare nisl. Nullam convallis augue justo, at imperdiet metus scelerisque quis.'
+
+const thirdBottomDiv = textContentInsideBottomContent[2];
+const thirdBottomHeading = thirdBottomDiv.querySelector('h4');
+thirdBottomHeading.textContent = 'Vision';
+const thirdBottomP = thirdBottomDiv.querySelector('p');
+thirdBottomP.textContent= 'Vision content elementum magna eros, ac posuere elvit tempus et. Suspendisse vel tempus odio, in interdutm nisi. Suspendisse eu ornare nisl. Nullam convallis augue justo, at imperdiet metus scelerisque quis.';
+
+const sectionClassContact = document.querySelector('.contact');
+const contactHeading = sectionClassContact.querySelector('h4');
+contactHeading.textContent = 'Contact';
+
+const contactPAll = sectionClassContact.querySelectorAll('p');
+const contactFirstP = contactPAll[0];
+contactFirstP.textContent = '123 Way 456 Street Somewhere, USA';
+const contactSecondP = contactPAll[1];
+contactSecondP.textContent = '1 (888) 888-8888';
+const contactThirdP = contactPAll[2];
+contactThirdP.textContent = 'sales@greatidea.io';
+
+const anchorTagInFooterTag = document.querySelector('footer a');
+anchorTagInFooterTag.classList = 'bold';
+anchorTagInFooterTag.textContent = siteContent.footer.copyright;
+
